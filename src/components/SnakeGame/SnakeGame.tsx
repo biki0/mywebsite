f9851c6ef37aa598c556500cc435df507ed1636e
biki0 <bikashkhatiwoda0@gmail.com>
2023-02-07 17:59:19 -0500

    open in new tab

--- src/components/SnakeGame/SnakeGame.tsx
+++ src/components/SnakeGame/SnakeGame.tsx
@@ -15,7 +15,7 @@ const SnakeGame = () => {
 
             <div style={ dStyle }>
             <p> COMING SOON (WHEN I FIGURE OUT HOW TO EMBED WASM WITH REACT TYPESCRIPT!!!</p>
-            <p>For now play it on -&gt; <a style={aStyle} href="https://dbofficial.us/">HERE</a>. It was made with C++.</p>
+            <p>For now play it on -&gt; <a target="_blank" style={aStyle} href="https://dbofficial.us/">HERE</a>. It was made with C++.</p>
             </div>
 
             
