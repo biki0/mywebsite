1b00fd54b00015727d2a04de08496f1d13f31934
biki0 <bikashkhatiwoda0@gmail.com>
2023-03-07 22:26:35 -0500

    code cleanup and added two's complement support

--- src/components/Converter/FixedPoint.tsx
+++ src/components/Converter/FixedPoint.tsx
@@ -8,136 +8,152 @@ const FixedPoint = () => {
     // let b_bits = number of bits after the decimal point of the fixed point value
 
     const [fixed_point_val, set_fp_value] = useState<string>("");
-    const [t_bits, set_t_bits] = useState<string>("");
-    const [b_bits, set_b_bits] = useState<string>("");
+    const [total_bits, set_total_bits] = useState<string>("");
+    const [bits_after_dec, set_bits_after_dec] = useState<string>("");
 
-    let binary_val: string = "0";
-    let hex_val: string = "0";
+    let binary_value: string = "";
+    let hex_value: string = "";
 
     const handle_fp_value = (event: React.ChangeEvent<HTMLInputElement>) => {
-
         set_fp_value(event.target.value);
-
     }
     
     const handle_t_bit_value = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-        set_t_bits(event.target.value);
-
+        set_total_bits(event.target.value);
     }
 
     const handle_b_bit_value = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-        set_b_bits(event.target.value);
-
+        set_bits_after_dec(event.target.value);
     }
 
     const integer_to_binary = (num: string, bit_amount: number) => {
-
         return parseInt(num, 10).toString(2).padStart(bit_amount, "0");
-
     }
-
+    
     const binary_to_hex = (bin_val: string) => {
-
         return parseInt(bin_val, 2).toString(16);
-
     }
 
-    //splits binary value starting from right to left into sections of 4bits 
-    //ex. {01010010 = 0101 0010 = 0x52} (4bits = 1hex digit)
-    const binary_sections_to_hex = (bin_val: string) => {
-       
+    //splits binary value starting from right to left into sections of 4 bits 
+    //ex. {01010010 = [0101, 0010] = 0x52} (4bits = 1hex digit)
+    const binary_sections_to_hex = (bin_val: string) => {       
+        
         let dec_bin_split: Array<string> = bin_val.split("");
         let dec_bin_arr: Array<string> = [];
 
-        while(dec_bin_split.length){
-
+        while(dec_bin_split.length) {
             dec_bin_arr.unshift(dec_bin_split.splice(-4).join(""));
-
         }
 
-        let h: string = "";
+        let hex_val: string = "";
 
-        for(let i=0; i<dec_bin_arr.length; ++i){
-
-            h = h + binary_to_hex(dec_bin_arr[i]).toUpperCase();
-
+        for(let i: number = 0; i < dec_bin_arr.length; ++i) {
+            hex_val = hex_val + binary_to_hex(dec_bin_arr[i]).toUpperCase();
         }
 
-        return h;
+        return "0x" + hex_val;
 
     }
 
-    const fixed_point_to_hex_bin = () => {
+    const fractions_to_binary = (fraction_val: number, total_frac_bits: number) => {
 
-        let fp: number = Number(fixed_point_val);
-        let t_b: number = Number(t_bits);
-        let b_b: number = Number(b_bits);
-        let bits_before_dec: number = t_b - b_b;
+        let binary_val: string = "";
+
+        for(let i: number = 0; i < total_frac_bits; ++i) {
+
+            if(!Number.isInteger(fraction_val)) {
+                fraction_val = fraction_val * 2;
+
+                if((fraction_val - 1) >= 0) {
+                
+                    fraction_val = fraction_val - 1;
+                    binary_val = binary_val + "1";
+                
+                }
+                else {
+                    binary_val = binary_val + "0";
+                }
+            }
+            else {
+                binary_val = binary_val + "0";
+            }
+
+        }
+
+        return binary_val;
+
+    }
+
+    const binary_twos_complement = (bin_val: string) => {
+
+        let bin_val_invert: string = bin_val.split('').map(x => x === "0" ? "1" : "0").join('');
+        let twos_comp: bigint = ((BigInt('0b' + bin_val_invert) + BigInt('0b' + 1)));
         
-        //have to divide the binary_vals into bits of 4 and then convert to hex. not do it all at once. same way its done for non whole numbers
-        if(Number.isInteger(fp)){
-            
-            let dec_bin_value: string = "0".repeat(b_b);
-            binary_val = integer_to_binary(fixed_point_val, bits_before_dec) + dec_bin_value;
-            hex_val = "0x" + binary_sections_to_hex(binary_val);
-            binary_val = binary_val + "." + "0".repeat(b_b);
-        
+        return twos_comp;
+
+    }
+
+    const fixed_point_conversion = (fp_val: number, t_bits: number, b_bits: number, is_twos_complement: boolean) : [bin_val: string, hex_val: string] => {
+
+        let bits_before_dec:number = t_bits - b_bits;
+        let bin_val: string = "";
+        let hex_val: string = "";
+        let int_part: number = Math.floor(fp_val);
+        let dec_part: number = fp_val - int_part;
+
+
+        if(Number.isInteger(fp_val)) {
+            bin_val = integer_to_binary(fp_val.toString(), bits_before_dec) + "0".repeat(b_bits);
         }
         else {
 
-            let int_part: number = Math.floor(fp);
-            let dec_part: number = fp - int_part;
-            let int_bin: string = integer_to_binary(int_part.toString(), bits_before_dec);
-            let dec_bin: string = "";
-            
-            let temp_b_bits: number = dec_part;
-   
-            for(let i=0; i<b_b; ++i) {
-                
-                if(!Number.isInteger(temp_b_bits)){
-       
-                    temp_b_bits = temp_b_bits * 2;
-
-                    if((temp_b_bits - 1) >= 0){
-
-                        temp_b_bits = temp_b_bits - 1;
-                        dec_bin = dec_bin + "1";
-
-                    }
-                    else{
-                        
-                        dec_bin = dec_bin + "0";
-
-                    }
-                    
-                }
-                else{
-                    
-                    dec_bin = dec_bin + "0".repeat(b_b - i);
-                    break
-
-                }
-
-            }
-
-            //0.26953125 <24,16> = 0x004500
-            //0.0019989013671875 <16,16> = 0x0083
-
-            let new_dec_bin: string = (t_b - b_b  == 0) ? dec_bin : int_bin + dec_bin;
-            hex_val = "0x" + binary_sections_to_hex(new_dec_bin);
-            binary_val = (t_b - b_b == 0) ?  " "+ "." + dec_bin : int_bin + "." + dec_bin;
+            let frac_bin: string = fractions_to_binary(dec_part, b_bits);
+            let whole_num_bin: string = integer_to_binary(int_part.toString(), bits_before_dec);
+            bin_val = (t_bits - b_bits == 0) ? frac_bin : whole_num_bin + frac_bin;
 
         }
 
+        if(is_twos_complement){
+            bin_val = binary_twos_complement(bin_val).toString(2);
+        }
+        
+        hex_val = binary_sections_to_hex(bin_val);
+        bin_val = bin_val.slice(0, bits_before_dec) + "." + bin_val.slice(bits_before_dec);
+
+        
+
+        return [bin_val, hex_val];
+
     }
 
-    if(fixed_point_val.length > 0 || t_bits.length > 0 || b_bits.length > 0) {
-        
-        fixed_point_to_hex_bin();
-    
+
+    if(fixed_point_val.length > 0 && total_bits.length > 0 && bits_after_dec.length > 0) {
+
+        let fp_value: number;
+        let t_bits: number = Number(total_bits);
+        let b_bits: number = Number(bits_after_dec);
+        let is_twos_complement: boolean;
+
+        if(fixed_point_val[0] == "-"){
+
+            fp_value = Number(fixed_point_val.replace("-", ""));
+            is_twos_complement = true;
+
+        }
+        else {
+
+            fp_value = Number(fixed_point_val);
+            is_twos_complement = false;
+
+        }
+
+        [binary_value, hex_value] = fixed_point_conversion(fp_value, t_bits, b_bits, is_twos_complement);
+
+        //0.26953125 <24,16> = 0x004500
+        //0.0019989013671875 <16,16> = 0x0083
+
     }
+
 
     return (
 
@@ -157,29 +173,29 @@ const FixedPoint = () => {
 
             <div className="card">
                 <div className="container">
-                    <h2><b>Binary: { binary_val } </b></h2>
+                    <h2><b>Binary: { binary_value } </b></h2>
                 </div>
             </div>
 
             <div className="card">
                 <div className="container">
-                    <h2><b>Hexadecimal: { hex_val }</b></h2>
+                    <h2><b>Hexadecimal: { hex_value }</b></h2>
                 </div>
             </div>
 
             <div className="input-boxes">
                 <h2> Fixed Point Value</h2>
-                <input className="fp-val" type="text" placeholder="ex: 0.0625" value={ fixed_point_val } onChange={ handle_fp_value } />
+                <input className="fp-val" type="text" placeholder="enter here" value={ fixed_point_val } onChange={ handle_fp_value } />
             </div>
         
             <div className="input-boxes">
                 <h2> Total Number of Bits</h2>
-                <input className="bit-val" type="text" placeholder="ex: 16" value={ t_bits } onChange={ handle_t_bit_value } />
+                <input className="bit-val" type="text" placeholder="enter here" value={ total_bits } onChange={ handle_t_bit_value } />
             </div>
 
             <div className="input-boxes">
                 <h2> Number of Bits After Decimal Point</h2>
-                <input className="dpoint-val" type="text" placeholder="ex: 14" value={ b_bits } onChange={ handle_b_bit_value } />
+                <input className="dpoint-val" type="text" placeholder="enter here" value={ bits_after_dec } onChange={ handle_b_bit_value } />
             </div>
 
         </>
